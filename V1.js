0cbd052864ab0cfcecbad4bf5a3b7cefcef27c27
Boris Conforty <boris@conforty.net>
2017-08-29 11:02:26 +0200

    Removed Facebook from list of auth types

--- V1.js
+++ V1.js
@@ -21,7 +21,7 @@ export type MyOpenFoodInstallationInfo = {
 
 export type MyOpenFoodUserInfo = {
   id?: number,
-  auth_type: 'email_password' | 'facebook' | 'anonymous',
+  auth_type: 'email_password' | 'anonymous',
   email?: string,
   password?: string,
   first_name?: string,
